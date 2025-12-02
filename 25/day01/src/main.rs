8ad11b0a3d975612a25215dbe163dc2ddc6d06b5
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-12-01 22:09:18 -0500

    chore: clean up day 1

--- 25/day01/src/main.rs
+++ 25/day01/src/main.rs
@@ -1,87 +1,82 @@
 use std::io;
 use utils::{Part, read_lines};
 
+pub type Instruction = (String, u32);
+
+const END_POS: u32 = 99;
+const START_POS: u32 = 50;
+
 fn main() -> io::Result<()> {
     let raw_lines = read_lines("input.txt")?;
-    let part_1 = get_password(Part::One, raw_lines.clone())?;
-    let part_2 = get_password(Part::Two, raw_lines)?;
+    let instructions = parse_lines(raw_lines);
+    let part_1 = get_password(Part::One, &instructions)?;
+    let part_2 = get_password(Part::Two, &instructions)?;
     println!("part 1: {part_1}");
     println!("part 2: {part_2}");
     Ok(())
 }
 
+fn get_password(part: Part, instructions: &Vec<Instruction>) -> io::Result<u32> {
+    let mut ring_buf = RingBuffer::new();
+    for i in instructions {
+        ring_buf.rotate(&part, &i.0, i.1);
+    }
+    Ok(ring_buf.zero_count)
+}
+
 struct RingBuffer {
     index: u32,
-    zero_sum: u32,
+    zero_count: u32,
 }
 
 impl RingBuffer {
     fn new() -> Self {
         RingBuffer {
-            index: 50,
-            zero_sum: 0,
+            index: START_POS,
+            zero_count: 0,
+        }
+    }
+
+    fn rotate(&mut self, part: &Part, direction: &str, distance: u32) {
+        for _ in 0..distance {
+            match direction {
+                "L" => self.left(),
+                "R" => self.right(),
+                s => panic!("expected L or R, got {}", s),
+            }
+            if let Part::Two = part {
+                self.check_and_increment();
+            }
+        }
+        if let Part::One = part {
+            self.check_and_increment();
         }
     }
 
     fn left(&mut self) {
         if self.index == 0 {
-            self.index = 99;
+            self.index = END_POS;
         } else {
             self.index -= 1;
         }
     }
 
     fn right(&mut self) {
-        if self.index == 99 {
+        if self.index >= END_POS {
             self.index = 0;
         } else {
             self.index += 1;
         }
     }
 
-    fn rotate_part1(&mut self, direction: String, distance: u32) {
-        for _ in 0..distance {
-            match direction.as_str() {
-                "L" => self.left(),
-                "R" => self.right(),
-                s => panic!("expected L or R, got {}", s),
-            }
-        }
+    fn check_and_increment(&mut self) {
         if self.index == 0 {
-            self.zero_sum += 1;
-        }
-    }
-
-    fn rotate_part2(&mut self, direction: String, distance: u32) {
-        for _ in 0..distance {
-            match direction.as_str() {
-                "L" => self.left(),
-                "R" => self.right(),
-                s => panic!("expected L or R, got {}", s),
-            }
-            if self.index == 0 {
-                self.zero_sum += 1;
-            }
+            self.zero_count += 1;
         }
     }
 }
 
-fn get_password(part: Part, raw_lines: Vec<String>) -> io::Result<u32> {
-    let lines = parse_lines(raw_lines);
-    let mut ring_buf = RingBuffer::new();
-
-    let op = match part {
-        Part::One => RingBuffer::rotate_part1,
-        Part::Two => RingBuffer::rotate_part2,
-    };
-    for line in lines {
-        op(&mut ring_buf, line.0, line.1);
-    }
-
-    Ok(ring_buf.zero_sum)
-}
-
-fn parse_lines(raw_lines: Vec<String>) -> Vec<(String, u32)> {
+fn parse_lines(raw_lines: Vec<String>) -> Vec<Instruction> {
     raw_lines
         .iter()
         .map(|l| {
